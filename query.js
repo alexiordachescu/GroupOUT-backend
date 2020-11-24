095d3e7d44e0caeaa0b793d9149b09df9ed05238
alexiordachescu <alex.iordachescu27@gmail.com>
2020-11-24 15:45:26 +0100

    add groupRouter to index.js

--- query.js
+++ query.js
@@ -1,12 +1,14 @@
 const User = require("./models").user;
 const Group = require("./models").group;
+const Tag = require("./models").tag;
 
 const getGroup = async () => {
   try {
-    const group = await Group.findByPk(1, {
+    const group = await Group.findByPk(2, {
       include: [
         { model: User, as: "owner" },
         { model: User, as: "member" },
+        { model: Tag },
       ],
     });
     console.log(group);
