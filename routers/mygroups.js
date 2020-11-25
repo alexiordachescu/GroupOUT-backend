cc53000edec1763c91a8b73a3871d49caedc6a96
alexiordachescu <alex.iordachescu27@gmail.com>
2020-11-25 16:28:19 +0100

    create the get /admin endpoint

---
+++ routers/mygroups.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const auth = require("../auth/middleware");
+const Group = require("../models").group;
+const User = require("../models").user;
+const Tag = require("../models").tag;
+const GroupMember = require("../models").groupMember;
+
+const router = new Router();
+
+router.get("/admin", auth, async (req, res) => {
+  try {
+    const loggedInUser = req.user.id;
+    const response = await Group.findAll({
+      where: { userId: loggedInUser },
+      include: [{ model: User, as: "member" }, { model: Tag }],
+    });
+    res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+module.exports = router;
