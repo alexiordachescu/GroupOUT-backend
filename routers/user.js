746c76820d3eab60ec0c19ac0be5bc73774e9276
alexiordachescu <alex.iordachescu27@gmail.com>
2020-11-27 11:42:37 +0100

    server responds with the updated user data

--- routers/user.js
+++ routers/user.js
@@ -1,16 +1,27 @@
 const { Router } = require("express");
 const auth = require("../auth/middleware");
-const Group = require("../models").group;
+
 const User = require("../models").user;
-const Tag = require("../models").tag;
-const GroupMember = require("../models").groupMember;
-const GroupComment = require("../models").groupComment;
 
 const router = new Router();
 
 router.patch("/:id", auth, async (req, res) => {
   const user = await User.findByPk(req.params.id);
-  console.log(user);
+
+  if (req.user.id !== user.id) {
+    return res.status(403).send({
+      message: "You don't have the permission to perform this action",
+      message,
+    });
+  }
+  if (user === null) {
+    return res
+      .status(404)
+      .send({ message: "This user doesn't exist in our database", message });
+  }
+  const { firstName, lastName, description, email } = req.body;
+  await user.update({ firstName, lastName, description, email });
+  return res.status(200).send({ user });
 });
 
 module.exports = router;
