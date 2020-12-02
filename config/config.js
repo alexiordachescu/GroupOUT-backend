9b41d56a57a49c581276882cfd20dbcae875af18
alexiordachescu <alex.iordachescu27@gmail.com>
2020-12-02 15:22:58 +0100

    post-release setup

--- config/config.js
+++ config/config.js
@@ -3,7 +3,6 @@ module.exports = {
   development: {
     url: process.env.ELEPHANT_SQL,
     dialect: "postgres",
-    operatorsAliases: "0",
   },
   test: {
     username: "root",
@@ -13,10 +12,8 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    database: "DATABASE_URL",
+
     dialect: "postgres",
   },
 };
